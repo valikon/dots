c4235d879fddb9460c6e30681dffff775626f535
Valentin Nemirovsky <valentin.nemirovsky@gmail.com>
2022-12-28 12:00:13 +0100

    screen - new office monitors

--- screenlayout/.screenlayout/my-layout4.sh
+++ screenlayout/.screenlayout/my-layout4.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 429x2160 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal
