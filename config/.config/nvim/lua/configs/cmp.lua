a5b8a15c2b0b52d8675b7db590a968894d27eb73
Valentin Nemirovsky <valentin@nemirovsky.se>
2024-02-06 22:18:19 +0100

    nvim - cmp setup

---
+++ config/.config/nvim/lua/configs/cmp.lua
@@ -0,0 +1,170 @@
+------------------
+---- nvim.cmp ----
+------------------
+
+-- TODO: check mawkler dots for more finetuned config
+
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'L3MON4D3/LuaSnip',                    -- Snippets TODO: fix snippets loading
+    'saadparwaiz1/cmp_luasnip' ,           -- Snippets
+    'rafamadriz/friendly-snippets',        -- Snippets
+    'onsails/lspkind-nvim',                -- Completion menu icons
+    'hrsh7th/cmp-nvim-lsp',                -- LSP completion
+    'hrsh7th/cmp-buffer',                  -- Buffer completion
+    'hrsh7th/cmp-path',                    -- Path completion
+    'hrsh7th/cmp-cmdline',                 -- Command-line completion
+    'hrsh7th/cmp-nvim-lua',                -- Nvim builtins completion
+    'hrsh7th/cmp-nvim-lsp-signature-help', -- Signature
+  },
+  event = { 'InsertEnter', 'CmdlineEnter' },
+  config = function()
+    local visible_buffers = require('utils').visible_buffers
+    local noice_is_loaded = require('utils').noice_is_loaded
+
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+    local cmp_disabled = cmp.config.disable
+    local cmp_insert = { behavior = cmp.SelectBehavior.Insert }
+    local lspkind = require('lspkind')
+
+    vim.opt.completeopt = 'menuone,noselect'
+
+    local function cmp_map(rhs, modes)
+      if (modes == nil) then
+        modes = {'i', 'c'}
+      else if (type(modes) ~= 'table')
+        then modes = {modes} end
+      end
+      return cmp.mapping(rhs, modes)
+    end
+
+    local function toggle_complete()
+      return function()
+        if cmp.visible() then
+          cmp.close()
+        else
+          cmp.complete()
+        end
+      end
+    end
+
+    local function complete()
+      if cmp.visible() then
+        cmp.mapping.confirm({select = true})()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      else
+        cmp.complete()
+      end
+    end
+
+    local function cmdline_complete()
+      if cmp.visible() then
+        cmp.mapping.confirm({select = true})()
+      else
+        cmp.complete()
+      end
+    end
+
+    local sources = {
+      { name = 'nvim_lsp' },
+      { name = 'luasnip', max_item_count = 5 },
+      { name = 'nvim_lua' },
+      { name = 'path', option = { trailing_slash = true } },
+      { name = 'buffer',
+        max_item_count = 3,
+        keyword_length = 2,
+        option = {
+          get_bufnrs = visible_buffers, -- Suggest words from all visible buffers
+        },
+      }
+    }
+    if not noice_is_loaded() then
+      -- Noice has its own signature help
+      table.insert(sources, { name = 'nvim_lsp_signature_help' })
+    end
+
+    -- LSPKind
+    lspkind.init({
+      symbol_map = require('utils.icons').icons
+    })
+
+    -- Places icon to the left, with margin
+    local function cmp_formatting()
+      return function(entry, vim_item)
+        local format_opts = { mode = 'symbol_text', maxwidth = 50 }
+        local kind = lspkind.cmp_format(format_opts)(entry, vim_item)
+        local strings = vim.split(kind.kind, '%s', { trimempty = true })
+
+        kind.kind = strings[1] or ''
+        kind.menu = '  ' .. (strings[2] or '')
+
+        return kind
+      end
+    end
+
+    cmp.PreselectMode = true
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = {
+        ['<C-j>'] = cmp_map(cmp.mapping.select_next_item(cmp_insert)),
+        ['<C-k>'] = cmp_map(cmp.mapping.select_prev_item(cmp_insert)),
+        ['<C-b>'] = cmp_map(cmp.mapping.scroll_docs(-4)),
+        ['<C-f>'] = cmp_map(cmp.mapping.scroll_docs(4)),
+        ['<C-Space>'] = cmp_map(toggle_complete(), {'i', 'c', 's'}),
+        ['<Tab>'] = cmp.mapping({
+          i = complete,
+          c = cmdline_complete,
+        }),
+        ['<C-y>'] = cmp_disabled,
+        ['<C-n>'] = cmp_disabled,
+        ['<C-p>'] = cmp_disabled,
+      },
+      sources = cmp.config.sources(sources),
+      window = {
+        completion = {
+          col_offset = -2, -- To fit lspkind icon
+          side_padding = 1, -- One character margin
+        },
+      },
+      formatting = {
+        fields = { 'kind', 'abbr', 'menu' },
+        format = cmp_formatting(),
+      },
+      completion = {
+        completeopt = 'menu,menuone,noinsert',
+      }
+    })
+
+    -- Use buffer source for `/` (searching)
+    cmp.setup.cmdline('/', {
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- Use cmdline & path source for `:`
+    cmp.setup.cmdline(':', {
+      sources = cmp.config.sources(
+        {
+          { name = 'path' }
+        },
+        {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' }
+            },
+          },
+        }
+      )
+    })
+  end
+}
--- config/.config/nvim/lua/configs/telescope.lua
+++ config/.config/nvim/lua/configs/telescope.lua
@@ -1,17 +1,15 @@
 -- Fuzzy Finder (files, lsp, etc)
 return {
   'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      -- NOTE: If you are having trouble with this installation,
-      --       refer to the README for telescope-fzf-native for more instructions.
+    { 'nvim-lua/plenary.nvim' },
+    { 
+      'nvim-telescope/telescope-fzf-native.nvim', 
       build = 'make',
-      cond = function()
-      return vim.fn.executable 'make' == 1
-      end,
+      config = function()
+        require("telescope").load_extension("fzf")
+      end
     },
   },
+  event = 'VeryLazy'
 }
