70f68d6600b7193519bb0d650a0bc3fe792f7d55
Valentin Nemirovsky <valentin.nemirovsky@gmail.com>
2025-12-22 06:21:17 +0100

    nvim - add comments

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -11,36 +11,36 @@ require('utils.lazy')
 local plugins = {
   'folke/lazy.nvim',
   use 'auto-session',
-  use 'barbar',
-  use 'blink',
+  use 'barbar',     -- Sexiest buffer tabline
+  use 'blink',      -- Autocompletion
   use 'catppuccin', -- use 'tokyonight', -- use 'onedark',
-  use 'comment',
-  use 'conform',
-  use 'feline',
-  use 'gitsigns',
+  use 'comment',    -- Adds `comment` verb
+  use 'conform',    -- Autoformatting
+  use 'feline',     -- Statusline framework
+  use 'gitsigns',   -- Git status in sign column
   -- use 'hardtime',
   use 'lazygit',
-  use 'leap',
-  use 'lsp',
-  use 'mason',
+  use 'leap',         -- Move cursor anywhere
+  use 'lsp',          -- Built-in LSP
+  use 'mason',        -- LSP/DAP/etc. package manager
   use 'mini',
   use 'noice',
   use 'nvim-tree',
   use 'obsidian',
-  use 'oil',
-  use 'rustaceanvim',
+  use 'oil',          -- Single directory file browser
+  use 'rustaceanvim', -- rust-analyzer client
   use 'snacks',
-  use 'telescope',
+  use 'telescope',    -- Fuzzy finder
   use 'todos',
-  use 'treesitter',
-  use 'trouble',
+  use 'treesitter',   -- Abstract syntax tree
+  use 'trouble',      -- Nicer list of diagnostics
   use 'twilight',
-  use 'typescript',
+  use 'typescript',   -- Typescript LSP client wrapper
   use 'vim-maximizer',
   use 'vim-tmux-navigator',
   use 'zen-mode',
   { 'folke/which-key.nvim',  opts = {} },
-  { 'karb94/neoscroll.nvim', opts = {} },
+  { 'karb94/neoscroll.nvim', opts = {} }, -- Smooth scrolling animations
 }
 
 require('lazy').setup({
